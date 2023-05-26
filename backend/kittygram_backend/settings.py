cfc9411bc3ad7b0059b6214b252cd2b8a658e50e
Андрей Николаевич <andreykaspb@gmail.com>
2023-05-26 19:55:18 +0300

    15 спринт общие настройки 9

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '84.201.163.0', 'blog.crimeagkh.ru']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
